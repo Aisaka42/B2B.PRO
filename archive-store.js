a9e8128ccf65170179e2c8b0a327c81c7cc532fa
Aisaka42 <aisaka_@mail.ru>
2026-05-16 00:55:50 +0700

    Update weekly archive after delete rating_15.05.2026_S-26-42_Создание отдела дистанционных продаж.md

--- archive-store.js
+++ archive-store.js
@@ -1,6 +1,6 @@
 window.archiveRepoData = {
   "site": "project-svetofor-plus",
-  "version": "2026-05-15T17:55:46.401Z",
+  "version": "2026-05-15T17:55:50.378Z",
   "archiveDocs": [
     {
       "id": "checklist_15.05.2026_S-26-42_Создание отдела дистанционных продаж.md",
