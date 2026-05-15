6d7e1a77c320c623a30270dc54250895a8ff5b59
Aisaka42 <aisaka_@mail.ru>
2026-05-16 00:57:18 +0700

    Update weekly archive after delete rating_15.05.2026_S-26-41_Рост производительности территориальных менеджеров.md

--- archive-store.js
+++ archive-store.js
@@ -1,6 +1,6 @@
 window.archiveRepoData = {
   "site": "project-svetofor-plus",
-  "version": "2026-05-15T17:57:14.728Z",
+  "version": "2026-05-15T17:57:18.062Z",
   "archiveDocs": [
     {
       "id": "checklist_15.05.2026_S-26-42_Создание отдела дистанционных продаж.md",
