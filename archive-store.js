48911f9953012e807de8007f0e084a048cc7cce0
Aisaka42 <aisaka_@mail.ru>
2026-05-16 00:55:46 +0700

    Update weekly archive after delete rating_15.05.2026_S-26-41_Рост производительности территориальных менеджеров.md

--- archive-store.js
+++ archive-store.js
@@ -1,6 +1,6 @@
 window.archiveRepoData = {
   "site": "project-svetofor-plus",
-  "version": "2026-05-15T17:55:40.472Z",
+  "version": "2026-05-15T17:55:46.401Z",
   "archiveDocs": [
     {
       "id": "checklist_15.05.2026_S-26-42_Создание отдела дистанционных продаж.md",
@@ -57,20 +57,6 @@ window.archiveRepoData = {
       "mime": "application/octet-stream",
       "savedAt": "2026-05-15T17:45:22.797Z",
       "filePath": "./archive/weekly/project_protocol_15.05.2026_S-26-19_Новые доходы BIG_B2B.md"
-    },
-    {
-      "id": "rating_15.05.2026_S-26-41_Рост производительности территориальных менеджеров.md",
-      "name": "rating_15.05.2026_S-26-41_Рост производительности территориальных менеджеров.md",
-      "sourceName": "rating_15.05.2026_S-26-41_Рост производительности территориальных менеджеров.md",
-      "type": "rating",
-      "projectCode": "S-26-41",
-      "projectName": "Рост производительности территориальных менеджеров",
-      "periodDate": "15.05.2026",
-      "ext": "md",
-      "size": 8009,
-      "mime": "application/octet-stream",
-      "savedAt": "2026-05-15T17:44:50.295Z",
-      "filePath": "./archive/weekly/rating_15.05.2026_S-26-41_Рост производительности территориальных менеджеров.md"
     },
     {
       "id": "rating_15.05.2026_S-26-27_Новые продукты B2B.md",
